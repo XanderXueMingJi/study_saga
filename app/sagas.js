ba1a3c0acbdf05bc4fdfd7817e09fcb255ae99a4
XueMingJi <xueming.ji@qq.com>
2018-04-24 17:24:22 +0800

    个人小理解

--- app/sagas.js
+++ app/sagas.js
@@ -5,19 +5,25 @@ export function* helloSaga() {
   console.log('Hello Saga!')
 }
 
+export function* xueSaga() {
+  console.log('Hello Xue ming ji!')
+}
+
 export function* incrementAsync() {
-  yield call(delay, 1000)
-  yield put({type: 'INCREMENT'})
+  console.log('hello');
+  yield call(delay, 1000) //delay  延迟    延迟1秒后下面的代码再操作
+  yield put({type: 'INCREMENT'}) // 个人觉得是 触发 action 的 INCREMENT
 }
 
 export function* watchIncrementAsync() {
-  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)//takeEvery:个人觉得是监听 action 触发 INCREMENT_ASYNC
 }
 
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
   yield [
     helloSaga(),
+    xueSaga(),
     watchIncrementAsync()
   ]
 }
